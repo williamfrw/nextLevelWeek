082383cf2c24a69ba58612b12fb3a60b33d61661
William Wang <53231921+williamfrw@users.noreply.github.com>
2020-08-12 18:55:55 -0300

    Update page.js

--- src/page.js
+++ src/page.js
@@ -1,67 +1,73 @@
 const dataBase = require('./database/db')
-const{subjects, weekdays, getSubject, convertHoursToMinutes} = require('.utils/format')
-const{prototype} = require('sqlite-async')
-const { catch, catch } = require('./database/db')
+const {subjects, weekdays, getSubject, convertHoursToMinutes} = require('./utils/format')
+const { prototype } = require('sqlite-async')
 
-function pageLanding(req, res) {
+function pageLanding (req, res){
     return res.render("index.html")
 }
+ 
+async function pageStudy(req, res){
+    const filters = req.query
 
-async function pageStudy(req, res) {
-    const filtes = req.query
-
-    if (!filtes.subject || !filters.weekday || !filters.time) {
+    if(!filters.subject || !filters.weekday || !filters.time){
         const proffys = ""
-        return res.render("study.html", {proffys, filters, subjects, weekdays})
+        return res.render("study.html", {proffys,filters, subjects, weekdays })
     }
-    
-    const timeToMinutes = convertHoutsToMinutes(filters.time)
-    console.log(filters.time + "em minutos: " + timeToMinutes)
+
+    const timeToMinutes = convertHoursToMinutes(filters.time)
+    console.log(filters.time + " em minutos: " + timeToMinutes)
 
     const query = `
         SELECT classes.*,proffys.*
         FROM proffys
         JOIN classes ON (classes.proffy_id = proffys.id)
         WHERE EXISTS(
-            SELECT class_schedule
+            SELECT class_schedule.*
+            FROM class_schedule
             WHERE class_schedule.class_id = classes.id
-            AND class_schedule.weekday = ${filtes.weekday}
+            AND class_schedule.weekday = ${filters.weekday}
             AND class_schedule.time_from <= ${timeToMinutes}
-            AND class_schedule.time_to > ${timeToMinutes}
-        )
-        AND classes.subject = "${filtes.subject}"
+            AND class_schedule.time_to > ${timeToMinutes}  
+        )          
+        AND classes.subject = "${filters.subject}"
     `
 
     try {
-        const db = await dataBaseconst proffys = await db.all(query)
+        const db = await dataBase
+        const proffys = await db.all(query)
         proffys.map((p) => {
-            proffys.subject = getSubject(proffy.subject)
+            proffy.subject = getSubject(proffy.subject)
         })
-        return res.render("study.html", {proffys, filtes, subjects, weekdays})
-    } catch(error) {
-        console.log("error: " + error)
-    }
+        return res.render("study.html", {proffys, filters, subjects, weekdays })
+    } catch (error) {
+        console.log("error:"+error)         
+    } 
 }
 
-function pageGiveClasses(req, res) {
+function pageGiveClasses(req, res){
     return res.render("give-classes.html", {subjects, weekdays})
 }
 
-function pageSuccessInsert(req, res) {
-    const createProffy = requiere('./database/createProffy')
+function pageSuccessInsert(req, res){
+    const filters = req.query
+    return res.render("success-insert.html", {filters})
+}
+
+async function saveClasses (req, res){
+    const createProffy = require('./database/createProffy')
     const proffyValue = {
-        name: req.body.name, 
+        name: req.body.name,
         avatar: req.body.avatar,
         whatsapp: req.body.whatsapp,
         bio: req.body.bio
     }
 
     const classValue = {
-        subject: req.body.subject
+        subject: req.body.subject,
         cost: req.body.cost
     }
 
-    const classScheduleValues = req.body.weekday.map((weekdat, index) => {
+    const classScheduleValues = req.body.weekday.map((weekday, index) => {
         return {
             weekday: weekday,
             time_from: convertHoursToMinutes(req.body.time_from[index]),
@@ -69,13 +75,16 @@ function pageSuccessInsert(req, res) {
         }
     })
 
+
     try {
-        const db = await dataBasecreateProffy(db, {proffyValue, classValuem classScheduleValues})
-        let querryString = "?subject="+req.body.subject
-        queryString +="&weekday="+req.body.weekday[0]
-        queryString += "&time="+req.body.time_from[0]
-        return res.redirect("/success-insert + queryString")
-    } catch(error) {
+        const db = await dataBase
+        createProffy(db, {proffyValue, classValue, classScheduleValues})
+        let queryString = "?subject="+req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[0]
+        return res.redirect("/success-insert" + queryString)
+            
+    } catch (error) {
         console.log(error)
     }
 }
